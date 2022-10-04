ef7fc2dd699e82e834bd563582b3c4e9bdfb06b2
Lachlan3 <lachlan.douglas1@my.jcu.edu.au>
2022-10-05 00:20:36 +1000

    Add random score generator and checker

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,6 +7,18 @@ Broken program to determine score status
 def main():
     score = float(input("Enter score: "))
     print(users_score(score))
+    continue_result = input("Generate Random Score (Y/N)? ")
+    while continue_result != "N" and continue_result != "Y":
+        print("Invalid Input")
+        continue_result = input("Generate Random Score (Y/N)? ")
+    if continue_result != "N":
+        import random
+        score = random.randint(0, 100)
+        print("The Random Score is", score)
+        print("The Result is", users_score(score))
+        print("Thank You.")
+    else:
+        print("Finished.")
 
 
 def users_score(score):
